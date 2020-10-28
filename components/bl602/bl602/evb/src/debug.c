a2d7b1b36c6936f50840aea33f452e2b27e173bc
Robert Lipe <robertlipe@gpsbabel.org>
2020-10-28 17:36:10 -0500

    In debug.c, don't compile code we aren't calling.
    ee_buf, cpy, parse_float,  decimal_point, cropzeros, flt are used only
    if DISABLE_PRINT_FLOAT. This provable since they're static in this file.
    There is no reason to compile them if DISABLE_PRINT_FLOAT is set, which
    is true in the default 602 build.

--- components/bl602/bl602/evb/src/debug.c
+++ components/bl602/bl602/evb/src/debug.c
@@ -292,8 +292,9 @@ char *fcvtbuf(double arg, int ndigits, int *decpt, int *sign, char *buf)
   return cvt(arg, ndigits, decpt, sign, buf, 0);
 }
 
-#ifndef DISABLE_PRINT_FLOAT
-static void ee_bufcpy(char *pd, char *ps, int count) {
+static void ee_bufcpy(char *d, char *s, int count); 
+ 
+void ee_bufcpy(char *pd, char *ps, int count) {
 	char *pe=ps+count;
 	while (ps!=pe)
 		*pd++=*ps++;
@@ -499,7 +500,6 @@ static char *flt(char *str, double num, int size, int precision, char fmt, int f
 
   return str;
 }
-#endif //  DISABLE_PRINT_FLOAT
 
 
 /*use O0 preventing consuming more stack*/
